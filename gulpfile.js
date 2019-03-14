6181ed9725f5b6770a1aa1f484e95cc1dae5416e
Oliver Tuck <olivertuckwebdev@gmail.com>
2019-03-14 00:00:08 +0000

    Updated layout and added .prettierrc

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const cssnano = require('gulp-cssnano');
 const imagemin = require('gulp-imagemin');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'js', 'images'], function () {
+gulp.task('serve', ['sass', 'js', 'images'], function() {
   browserSync.init({
     server: 'dist'
   });
@@ -19,36 +19,37 @@ gulp.task('serve', ['sass', 'js', 'images'], function () {
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function () {
-  return gulp.src('src/scss/main.scss')
+gulp.task('sass', function() {
+  return gulp
+    .src('src/scss/main.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: [
-        'last 2 versions',
-        'android 4',
-        'opera 12'
-      ]
-    }))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions', 'android 4', 'opera 12']
+      })
+    )
     .pipe(gulp.dest('dist/css'))
     .pipe(cssnano())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.stream({match: '**/*.css'}));
+    .pipe(browserSync.stream({ match: '**/*.css' }));
 });
 
 // JS
-gulp.task('js', function () {
-  return gulp.src('src/js/*.js')
+gulp.task('js', function() {
+  return gulp
+    .src('src/js/*.js')
     .pipe(gulp.dest('dist/js'))
     .pipe(browserSync.stream());
 });
 
 // Optimize images
-gulp.task('images', function () {
-  gulp.src('src/images/*')
+gulp.task('images', function() {
+  gulp
+    .src('src/images/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('default', ['serve']);
